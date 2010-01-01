c5958f2321f6cae67604ba82e537e2042397c526
Andrew Wright <ayax79@gmail.com>
2009-12-31 17:02:28 -0800

    Added comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
   validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
   validates_length_of       :name,     :maximum => 100
 
+
+#  will add these in later, commented out because email isn't always available on openid/oauth response
 #  validates_presence_of     :email
 #  validates_length_of       :email,    :within => 6..100 #r@a.wk
 #  validates_uniqueness_of   :email
